b3bcd8a4f2bf25724716ed4cc0cddf620b28132a
AimeeJay <ajesso@post.com>
2021-08-29 15:35:52 -0400

    Installed and imported chart.js and the ridiculously large pie graphs are now working in the app

--- aimeejreactpf/src/pages/Home.js
+++ aimeejreactpf/src/pages/Home.js
@@ -1,8 +1,32 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React, { useState } from 'react';
+import { Pie, Doughnut } from 'react-chartjs-2';
 
 function Home() {
-
+    const state = {
+        labels: ['January', 'February', 'March',
+            'April', 'May'],
+        datasets: [
+            {
+                label: 'Rainfall',
+                backgroundColor: [
+                    '#B21F00',
+                    '#C9DE00',
+                    '#2FDE00',
+                    '#00A6B4',
+                    '#6800B4'
+                ],
+                hoverBackgroundColor: [
+                    '#501800',
+                    '#4B5000',
+                    '#175000',
+                    '#003350',
+                    '#35014F'
+                ],
+                data: [65, 59, 80, 81, 56]
+            }
+        ]
+    }
     return (
         <div>
             <section className="hero">
@@ -15,7 +39,37 @@ function Home() {
                 </div>
             </section>
             <section id="skillset">
-                
+                <div>
+                    <Pie
+                        data={state}
+                        options={{
+                            title: {
+                                display: true,
+                                text: 'Average Rainfall per month',
+                                fontSize: 20
+                            },
+                            legend: {
+                                display: true,
+                                position: 'right'
+                            }
+                        }}
+                    />
+
+                    <Doughnut
+                        data={state}
+                        options={{
+                            title: {
+                                display: true,
+                                text: 'Average Rainfall per month',
+                                fontSize: 20
+                            },
+                            legend: {
+                                display: true,
+                                position: 'right'
+                            }
+                        }}
+                    />
+                </div>
             </section>
         </div>
     )
